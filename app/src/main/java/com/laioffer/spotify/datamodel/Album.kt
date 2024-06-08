8ce92e8523576038206868e89e4feb70d0f75108
Renjie Liu <143575423+renjieliu0729@users.noreply.github.com>
2024-06-07 23:50:11 -0500

    Update Album.kt

--- app/src/main/java/com/laioffer/spotify/datamodel/Album.kt
+++ app/src/main/java/com/laioffer/spotify/datamodel/Album.kt
@@ -1,3 +1,33 @@
 package com.laioffer.spotify.datamodel
 
-data class Album()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+@Entity
+data class Album(
+    @PrimaryKey
+    val id: Int,
+    @SerializedName("album")
+    val name: String,
+    val year: String,
+    val cover: String,
+    val artists: String,
+    val description: String
+    // all fileds must be serializable
+): Serializable {
+    // similar to java static function
+    companion object {
+        fun empty(): Album {
+            return Album(
+                id = -1,
+                "",
+                "",
+                "",
+                "",
+                "",
+            )
+        }
+    }
+}
