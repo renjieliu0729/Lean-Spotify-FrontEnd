97344e0d1fc5547d090fd7ded33c2d4cc29b292e
Renjie Liu <143575423+renjieliu0729@users.noreply.github.com>
2024-06-07 23:49:17 -0500

    Update AppDatabase.kt

--- app/src/main/java/com/laioffer/spotify/database/AppDatabase.kt
+++ app/src/main/java/com/laioffer/spotify/database/AppDatabase.kt
@@ -1,4 +1,10 @@
 package com.laioffer.spotify.database
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.laioffer.spotify.datamodel.Album
+
+@Database(entities = [Album::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun databaseDao(): DatabaseDao
 }
