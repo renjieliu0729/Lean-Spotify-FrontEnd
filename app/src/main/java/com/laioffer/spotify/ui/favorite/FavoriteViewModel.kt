5acb1fcf49a0c1ff929efae0cca2f7215ac583ac
Renjie Liu <143575423+renjieliu0729@users.noreply.github.com>
2024-06-08 13:52:02 -0500

    Update FavoriteViewModel.kt

--- app/src/main/java/com/laioffer/spotify/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/laioffer/spotify/ui/favorite/FavoriteViewModel.kt
@@ -1,4 +1,34 @@
 package com.laioffer.spotify.ui.favorite
 
-class FavoriteViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.laioffer.spotify.datamodel.Album
+import com.laioffer.spotify.repository.FavoriteAlbumRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class FavoriteViewModel @Inject constructor(
+    private val favoriteAlbumRepository: FavoriteAlbumRepository
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(FavoriteUiState(emptyList()))
+    val uiState: StateFlow<FavoriteUiState> = _uiState
+
+    init {
+        viewModelScope.launch {
+            favoriteAlbumRepository.fetchFavoriteAlbums().collect {
+                _uiState.value = FavoriteUiState(it)
+            }
+        }
+    }
 }
+
+// data and variables that will be used in view
+data class FavoriteUiState(
+    val albums: List<Album>
+)
