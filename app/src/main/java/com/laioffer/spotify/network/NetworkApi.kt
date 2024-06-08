3e31e09514f58b042e84d810ed6f8dd046e0b7e0
Renjie Liu <143575423+renjieliu0729@users.noreply.github.com>
2024-06-07 23:50:55 -0500

    Update NetworkApi.kt

--- app/src/main/java/com/laioffer/spotify/network/NetworkApi.kt
+++ app/src/main/java/com/laioffer/spotify/network/NetworkApi.kt
@@ -1,4 +1,18 @@
 package com.laioffer.spotify.network
 
-class NetworkApi {
+import com.laioffer.spotify.datamodel.Playlist
+import com.laioffer.spotify.datamodel.Section
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface NetworkApi {
+    @GET("feed")
+    // promise similar 
+    fun getHomeFeed(): Call<List<Section>>
+
+    // get http://0.0.0.0:8080/playlist/1
+    @GET("playlist/{id}")
+    fun getPlaylist(@Path("id") id: Int): Call<Playlist>
 }
+
